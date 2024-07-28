3c1633852b806bff4162b9dad5b9fda161133ee9
Gabe De Mesa <gabedemesa@Gabes-MacBook-Pro.local>
2024-07-28 05:26:09 -0700

    fix: verbeage

--- script.js
+++ script.js
@@ -388,8 +388,8 @@ const createHeatmapChart = (data, selector) => {
 
 // Chart 3: Stacked Bar Chart
 const createStackedBarChart = (data, selector) => {
-    const margin = {top: 20, right: 20, bottom: 50, left: 90};
-    const width = 1000 - margin.left - margin.right;
+    const margin = {top: 50, right: 20, bottom: 50, left: 90};
+    const width = 1100 - margin.left - margin.right;
     const height = 500 - margin.top - margin.bottom;
 
     const svg = createSVG(selector, width, height, margin);
